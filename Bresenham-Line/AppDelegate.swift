a1595a77dcde3a5e1d56b48b0af441cc35b51d1e
CMK <cirno.mainasuk@gmail.com>
2017-03-17 19:35:11 +0800

    Initial Commit

---
+++ Bresenham-Line/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Bresenham-Line
+//
+//  Created by Cirno MainasuK on 2017-3-17.
+//  Copyright © 2017年 Cirno MainasuK. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Bresenham-Line/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Bresenham-Line
+//
+//  Created by Cirno MainasuK on 2017-3-17.
+//  Copyright © 2017年 Cirno MainasuK. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
